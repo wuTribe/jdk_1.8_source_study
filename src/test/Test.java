b0795dd94c3a6304fc1ce83af648c290ce33c2c0
Wu <907871429@qq.com>
2020-04-18 17:01:09 +0800

    创建添加jdk源码文件

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<String, Double> map = new HashMap<>();
+        map.put("k1", 0.1);
+        System.out.println(map);
+    }
+}
